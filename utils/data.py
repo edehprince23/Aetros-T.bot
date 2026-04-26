2effb92df67610fe69956ad0d0054d1abacf349e
edehprince23 <edehprince23@gmail.com>
2026-04-26 13:12:28 +0100

    data.py

---
+++ utils/data.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+
+def get_candles(symbol, interval="1m", limit=100):
+    url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}"
+    data = requests.get(url).json()
+
+    df = pd.DataFrame(data, columns=[
+        "time","open","high","low","close","volume",
+        "close_time","qav","trades","tbbav","tbqav","ignore"
+    ])
+
+    df["close"] = df["close"].astype(float)
+    df["open"] = df["open"].astype(float)
+    df["high"] = df["high"].astype(float)
+    df["low"] = df["low"].astype(float)
+
+    return df
